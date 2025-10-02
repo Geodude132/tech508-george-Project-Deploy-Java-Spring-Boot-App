ee4f00fb7a03abac008473b788b406d073b09bb3
George Merrison <georgevicitunes@hotmail.co.uk>
2025-10-02 12:40:26 +0100

    Kubernetes task v1

---
+++ LibraryProject2/k8s/Kubernetes_user_data.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euo pipefail
+
+# Update system
+apt update -y && apt upgrade -y
+
+# Install dependencies
+apt install -y curl apt-transport-https docker.io conntrack git
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Install Minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+install minikube-linux-amd64 /usr/local/bin/minikube
+
+# Clone your repo
+cd /home/ubuntu
+git clone https://github.com/Geodude132/tech508-george-Project-Deploy-Java-Spring-Boot-App.git ProjectLibrary2
+cd ProjectLibrary2/k8s
+
+# Start Minikube
+minikube start --driver=docker
+
+# Apply Kubernetes YAML files
+kubectl apply -f mysql-deployment.yaml
+kubectl apply -f app-deployment.yaml
+kubectl apply -f app-service.yaml
